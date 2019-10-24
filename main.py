2146449ca15b6273ddd255540c3a2812765cf10a
IvanKMK <kumeyko@hotmail.com>
2019-10-24 23:39:38 +0200

    lesson1_step10

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from jinja2 import Environment, FileSystemLoader, select_autoescape
 import datetime
 
 START_YEAR = 1920
+ROSAS_FILE = 'vino.txt'
 
 env = Environment(
     loader=FileSystemLoader('.'),
@@ -11,8 +12,16 @@ env = Environment(
 template = env.get_template('template.html')
 
 rendered_page = template.render(
-    how_old = datetime.datetime.now().year - START_YEAR
+    how_old=datetime.datetime.now().year - START_YEAR
 )
 
-with open('index.html', 'w', encoding="utf8") as file:
-    file.write(rendered_page)
+# with open('index.html', 'w', encoding="utf8") as file:
+#     file.write(rendered_page)
+
+with open(ROSAS_FILE, "r") as my_file:
+    file_contents = [wine.split(':') for wine in my_file.read().split(
+        '\n') if wine != '']
+
+    for attr, value in file_contents:
+        if attr == 'Сорт':
+            print(value)
